0d9f4cb378a0c46cffc541dd7df240746acbf049
Mavia Khalid <125903727+Mavia-Khalid@users.noreply.github.com>
2023-06-11 22:06:46 +0530

    Add files via upload

--- BST.cpp
+++ BST.cpp
@@ -171,6 +171,80 @@ public:
         }
         return GetSize(root->left) + GetSize(root->right) + 1;
     }
+    int Min(node* root)
+    {
+        if(root == NULL)
+        {
+            return -1;
+        }
+        int min = root->data;
+        while(root->left != NULL)
+        {
+            root = root->left;
+        }
+        return root->data;
+    }
+    int Max(node* root)
+    {
+        if(root == NULL)
+        {
+            return -1;
+        }
+        int max = root->data;
+        while(root->right != NULL)
+        {
+            root = root->right;
+        }
+        return root->data;
+    }
+    node* deleteNodeInBST(node* root, int target) {
+	
+	//base cae
+	if(root == NULL ) {
+		return NULL;
+	}
+//cout << "Request recieved for " >> root->data << " with target" << target << endl;
+	if(root->data == target) {
+		//isi ko delete krna h 
+		//4 cases 
+		if(root->left == NULL && root->right == NULL) {
+			//leaf node
+			//delete root;
+			return NULL;
+		}
+		else if(root->left == NULL && root->right != NULL) {
+			node* child = root->right;
+			//delete root;
+			return child;
+		}
+		else if(root->left != NULL && root->right == NULL) {
+			node* child  = root->left;
+			//delete root;
+			return child;
+		}
+		else {
+			//both child
+			//find inorder predecessor inb left subtree
+			int inorderPre = Max(root->left);
+			//replace root->data value with inorder predecessor
+			root->data = inorderPre;
+			//delete inorder predecessor from left subtree
+			root->left = deleteNodeInBST(root->left,inorderPre);
+			return root;
+			
+		}
+		
+	}
+	else if(target > root -> data) {
+		//right jana chahiye
+		root->right =  deleteNodeInBST(root->right, target);
+	}
+	else if(target < root->data) {
+		//left jana chahioye
+		root->left = deleteNodeInBST(root->left, target);
+	}
+	return root;
+}
 };
 int main()
 {
@@ -186,5 +260,11 @@ int main()
     cout << "\n\t Height : " << tree.height(tree.root);
     cout << "\n\t Size : " << tree.GetSize(tree.root);
     cout << endl;
+    cout << "\n\t Min : " << tree.Min(tree.root);
+    cout << "\n\t Max : " << tree.Max(tree.root);
+    cout << "\n\t Deleting 15 : ";
+    tree.deleteNodeInBST(tree.root,15);
+    cout << "\n\t After Deletion : ";
+    tree.PrintInOrder();
     return 0;
 }
